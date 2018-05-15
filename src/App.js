562220bde89f95d077fcea78a14a0650d1838818
tmt <thomas.culdaut@gmail.com>
2018-05-15 11:07:01 +0200

    version onClick

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button>-</button>
+        <button onClick={ () => this.setState({
+          count: this.state.count + 1
+        })} >+</button>
+      </div>
+    );
+  }
+}
+
+export default App;
